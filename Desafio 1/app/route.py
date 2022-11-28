b6970d5974849b087f597515d930f8090e39bb62
NilberSiqueira <nilbernick7@gmail.com>
2022-11-28 00:45:32 -0300

    atualização

--- Desafio 1/app/route.py
+++ Desafio 1/app/route.py
@@ -1,12 +1,17 @@
 from app import app
 from flask import render_template
 
-@app.route('/')
-@app.route('/index')
-def index():
+@app.route("/")
+@app.route("/index")
+def home():
     return render_template("index.html")
 
+@app.route("/quem_somos")
+def quem_somos():
+    return render_template("quem_somos.html")
 
-@app.route('/contato')
-def contato():
+
+@app.route("/contato")
+def contatos():
     return render_template("contato.html")
+
